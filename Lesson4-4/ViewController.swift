b85bc2f9ce360ef100b6d7434f158635ec62992b
新井沙織 <araisaori@araisaori-no-MacBook-Air.local>
2017-04-30 13:32:58 +0900

    Initial Commit

---
+++ Lesson4-4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lesson4-4
+//
+//  Created by 新井沙織 on 2017/04/30.
+//  Copyright © 2017年 saori.arai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
